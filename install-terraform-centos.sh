82d9955febc3b6cb4e7d161986306d13d7edad0c
Muhammad Cordiaz <cordiaz@cordiaz.com>
2020-02-10 09:02:20 +0700

    modified setup_sosial_*.txt

---
+++ install-terraform-centos.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sudo yum update -y
+sudo wget sudo wget https://releases.hashicorp.com/terraform/0.12.2/terraform_0.12.20_linux_amd64.zip
+sudo unzip ./terraform_0.12.2_linux_amd64.zip –d /usr/local/bin
